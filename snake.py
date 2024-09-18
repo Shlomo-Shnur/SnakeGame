4d9ed84e705da8854943fd72f5ffd808e5784929
Shlomo <shlomoshnur@gmail.com>
2024-09-18 11:10:45 +0300

    small code beautifying changes to constants

--- snake.py
+++ snake.py
@@ -1,12 +1,11 @@
 from turtle import Turtle
 
-TURTLE_SIZE = 20
-SNAKE_HEAD = 0
-MOVE_DISTANCE = 20
+TURTLE_SIZE = MOVE_DISTANCE = 20
+SNAKE_HEAD = RIGHT = 0
+LAST_SEGMENT = -1
 UP = 90
 DOWN = 270
 LEFT = 180
-RIGHT = 0
 
 
 class Snake:
@@ -46,10 +45,10 @@ class Snake:
         if not self.snake_body:
             xcor = ycor = 0
         elif len(self.snake_body) < 3:
-            xcor = self.snake_body[-1].xcor() - 20
+            xcor = self.snake_body[LAST_SEGMENT].xcor() - 20
             ycor = 0
         else:
-            xcor, ycor = self.snake_body[-1].position()
+            xcor, ycor = self.snake_body[LAST_SEGMENT].position()
         new_segment = Turtle(shape="square")
         new_segment.penup()
         new_segment.color("white")
